f5c75f2692629c8246b20cb33bb3e5b1983b1380
sj970806 <sj970806@naver.com>
2023-01-19 14:48:43 +0000

    [Feat]: Diversity example plots (MKDIR-82)

--- frontend/pages/03_Single_Model_Analysis.py
+++ frontend/pages/03_Single_Model_Analysis.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import requests
-import pandas
-import plotly
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 
 st.set_page_config(layout="wide")
 
@@ -45,18 +46,48 @@ for i in range(int(n_recommends)):
     reranking_form = ReRanking_Form(key=before_key)
     before_key += reranking_form.key 
     forms.append(reranking_form.get_data())
-    
+
 st.sidebar.button('Compare!')
-    
-sma_plot1, sma_plot2 = st.columns(2)
+exp_div_df = pd.DataFrame(columns = ['model', 'diversity', 'mean', 'colors'])
+## TODO : '/Diversity_for_users'로 request 보내서 Diversity plot 그리기 위한 정보 받아오기
 
-sma_plot1.markdown('<h4>Serendipity</h4>', unsafe_allow_html=True)
+## TODO : for문으로 비교할만큼 모델 정보 받아와서 추가하기
+exp_div_df.loc[1, :] = ['M1',[0.4, 0.5, 0.5, 0.6, 0.6, 0.7], 0.5, 'red']
+exp_div_df.loc[2, :] = ['M2',[0.1, 0.2, 0.2, 0.3, 0.3, 0.4], 0.2, 'blue']
+exp_div_df.loc[3, :] = ['M3',[0.6, 0.7, 0.7, 0.8, 0.8, 0.9], 0.8, 'green']
 
-sma_plot1.markdown('<h4>Novelty</h4>', unsafe_allow_html=True)
+div_plot1, div_plot2 = st.columns(2)
 
-sma_plot2.markdown('<h4>Diversity</h4>', unsafe_allow_html=True)
+div_plot1.markdown('<h4>Diversity Histogram</h4>', unsafe_allow_html=True)
+fig = go.Figure()
+for i in exp_div_df.index:    
+    fig.add_trace(go.Histogram(
+        x=exp_div_df.loc[i,'diversity'],
+        marker={'color' : exp_div_df.loc[i,'colors']}
+     ))
+fig.update_traces(opacity=0.5)
+div_plot1.plotly_chart(fig, use_container_width=True)
 
-sma_plot2.markdown('<h4>Coverage</h4>', unsafe_allow_html=True)
+div_plot2.markdown('<h4>Diversity Bar plot</h4>', unsafe_allow_html=True)
+fig = px.bar(exp_div_df, x='model', y='mean', color='model', color_discrete_sequence=exp_div_df['colors'].values)
+div_plot2.plotly_chart(fig, use_container_width=True)
+
+## TODO : '/Serendipity_for_users'로 request 보내서 Serendipty plot 그리기 위한 정보 받아오기
+
+ser_plot1, ser_plot2 = st.columns(2)
+
+ser_plot1.markdown('<h4>Serendipity Histogram</h4>', unsafe_allow_html=True)
+
+ser_plot2.markdown('<h4>Serendipity Bar plot</h4>', unsafe_allow_html=True)
+
+## TODO : '/Novelty_for_users'로 request 보내서 Novelty plot 그리기 위한 정보 받아오기
+
+nov_plot1, nov_plot2 = st.columns(2)
+
+nov_plot1.markdown('<h4>Novelty Histogram</h4>', unsafe_allow_html=True)
+
+nov_plot2.markdown('<h4>Novelty Bar plot</h4>', unsafe_allow_html=True)
+
 
 st.markdown(f"<h2 style='text-align: left; color: black;'>Patterns by side-informations</h2>", unsafe_allow_html=True)
 
