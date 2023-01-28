3fb93ac0bf74dde25bdb8499e7035c78f622ed1a
donggun <donggun1000@naver.com>
2023-01-28 15:05:52 +0000

    [Feat] : Add embedding graph and make interactive (MKDIR-135)

--- frontend/dash_/pages/05_00_deep_analysis_item.py
+++ frontend/dash_/pages/05_00_deep_analysis_item.py
@@ -16,6 +16,7 @@ dash.register_page(__name__, path='/deep_analysis')
 
 user = pd.read_csv('/opt/ml/user.csv', index_col='user_id')
 item = pd.read_csv('/opt/ml/item.csv', index_col='item_id')
+item['selected'] = 0
 
 uniq_genre = set()
 for i in item['genre']:
@@ -77,14 +78,33 @@ sidebar = html.Div(
             className='form-style'),
         dcc.Store(id='item_selected', storage_type='session') #데이터를 저장하는 부분
     ],
-    # className='sidebar'
+    className='sidebar'
 )
+
+emb = px.scatter(
+    item,
+    x = item['xs'],
+    y = item['ys'],
+    # color=item['selected'],
+    opacity=0.5
+    )
+
+emb.update_layout(clickmode='event+select')
+emb.update_traces()
+
 
 embedding = html.Div(
     children=[
         html.H3('아이템 2차원 임베딩'),
         html.P('참고로 리랭킹 관련한 지원은 유저 페이지에서만 됩니다.'),
-        html.Br()
+        html.Br(),
+        dcc.Graph(
+            id='emb_graph',
+            figure=emb,
+            style={
+                'config.responsive': True
+            }
+            )
     ]
 )
 
@@ -92,9 +112,7 @@ side = html.Div(
     children=[
         html.H3('사이드인포'),
         html.Br(),
-        html.Div(
-            id='side_graph'
-        )
+        html.Div(id='side_graph')
     ]
 )
 
@@ -105,14 +123,22 @@ related_users = html.Div(
     ]
 )
 
+test = html.Div(
+    children=[
+        html.H3(id='test', children='dd')
+    ]
+)
+
 layout = html.Div(children=[
     gct.navbar,
     sidebar,
     embedding,
+    test,
     side,
     related_users,
 ],className='content')
 
+# 옵션으로 선택한 것이 store에 저장되게 함
 @callback(
     Output('item_selected', 'data'),
     Input('selected_genre', 'value'), Input('selected_year', 'value'),
@@ -120,18 +146,60 @@ layout = html.Div(children=[
 def store_selected_item(genre, year):
     if genre is None:
         raise PreventUpdate
+    if year is None:
+        raise PreventUpdate
+    # else:
+    #     item_lst = [i['pointIndex'] for i in emb['points']]
+    #     return item_lst
     tmp = item.copy()
     tmp = tmp[tmp['genre'].str.contains(
         ''.join([*map(lambda x: f'(?=.*{x})', genre)]) + '.*', regex=True)]
     tmp = tmp[(tmp['release_year'] >= year[0]) & (tmp['release_year'] <= year[1])]
     return tmp.index.to_list()
 
+# store에 저장된 것 or 임베딩에서 선택한 것이 사이드 그래프에 표시되게 함
 @callback(
     Output('side_graph', 'children'),
-    Input('item_selected', 'data')
+    Input('item_selected', 'data'),
+    Input('emb_graph', 'selectedData'), # 각 인풋으로 이 콜백이 작동하는데 작동시킨 인풋을 가려내고 싶다
 )
-def change_side_graph(item_id):
+def change_side_graph(item_id, emb):
     tmp = item.loc[item_id]
+    if emb is not None:
+        item_lst = [i['pointNumber'] for i in emb['points']]
+        tmp = item.iloc[item_lst]
     year = px.histogram(tmp, x='release_year')
     genre = px.histogram(tmp, x='genre')
-    return dcc.Graph(figure=year), dcc.Graph(figure=genre)
+
+    return (dcc.Graph(figure=year), dcc.Graph(figure=genre))
+
+# store에 저장된 것으로 임베딩에 색깔 표시함
+@callback(
+    Output('emb_graph', 'figure'),
+    Input('item_selected', 'data'),
+)
+def change_emb_graph(item_id):
+    item['selected'] = 'Not Selected'
+    item.loc[item_id, 'selected'] = 'Selected'
+    emb = px.scatter(
+        item, x = 'xs', y = 'ys', color='selected',
+        opacity=0.9,
+        marginal_x="histogram",
+        marginal_y="histogram",
+        )
+    emb.update_layout(clickmode='event+select')
+    emb.update_traces()
+
+    return emb
+
+# # 그래프에서 선택한 것도 store에 저장하게 함
+@callback(
+    Output('test', 'children'),
+    Input('emb_graph', 'selectedData')
+)
+def choose_in_graph(data):
+    item_lst = None
+    if data is not None:
+        item_lst = [i['pointIndex'] for i in data['points']]
+
+    return item_lst
