6eb592e918aa33aed8eacf0073eed9cab73c2910
mbaek01 <marcusbaek248@gmail.com>
2023-01-30 05:08:27 +0000

    [Feat]: get_exp() for returning experiments of selected dataset (MKDIR-144)

--- backend/routers/frontend.py
+++ backend/routers/frontend.py
@@ -1,3 +1,23 @@
-from fastapi import APIRouter
+from asyncmy.cursors import DictCursor
+from fastapi import APIRouter, Depends
+from typing import Dict
 
-router = APIRouter()
+from routers.database import get_db_dep
+
+router = APIRouter()  
+
+@router.get('/get_exp')
+async def get_exp_total(ID: str, dataset_name:str, connection=Depends(get_db_dep)) -> Dict:
+    async with connection as conn:
+        async with conn.cursor(cursor=DictCursor) as cur:
+            query = 'SELECT experiment_name, alpha, objective_fn, hyperparameters, \
+                     recall, map, ndcg, tail_per, avg_pop, coverage, \
+                     diversity_cos, diversity_jac, serendipity_pmi, serendipity_jac, novelty \
+                    FROM Experiments WHERE ID = %s AND dataset_name = %s'
+            await cur.execute(query, (ID, dataset_name))
+            result = cur.fetchall()
+
+    return result 
+    # [{'experiment_name': (name), 'alpha' : (alpha)}, {'experiment_name2': (name2), 'alpha2' : (alpha2)}, ...]
+
+
