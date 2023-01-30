9033d89d7d68940d2a2f3de3724587cd752fe4c2
donggun <donggun1000@naver.com>
2023-01-30 00:55:21 +0000

    [Refactor] : 05_00 resize of selection box (MKDIR-135)

--- frontend/dash_/pages/05_00_deep_analysis_item.py
+++ frontend/dash_/pages/05_00_deep_analysis_item.py
@@ -1,5 +1,5 @@
 import dash
-from dash import html, dcc, callback, Input, Output, State
+from dash import html, dcc, callback, Input, Output, State, ctx
 import dash_bootstrap_components as dbc
 import requests
 import pandas as pd
@@ -28,91 +28,64 @@ fig.update_layout(clickmode='event+select')
 
 fig.update_traces()
 
-sidebar = html.Div(
+
+selection = html.Div(
     children=[
-        html.Div(
-            children=[
-                html.P('다른 페이지에서 입력한 아이템'),
-                html.P('가령 유저 페이지에서 20대가 본 아이템들에 대한 분석을 하고 싶어서 넘어온 상황!'),
-                html.Button('취소!'),
-                html.P('그러면 아래 블록이 나옴(이 블록 사라지면서)'),
-                html.Br(),
-            ],
-            className='form-style'),
-        html.Div(
-            children=[
-                html.P('아니면 니가 선별해서 골라보라'),
-                html.Hr(),
-                # 년도, 장르, 제목?, 인기도
-                html.P('장르'),
-                dcc.Dropdown(
-                    options=[*uniq_genre],
-                    value=['Drama'],
-                    multi=True,
-                    id='selected_genre'
-                ),
-                html.P('년도'),
-                dcc.RangeSlider(
-                    min=item['release_year'].min(),
-                    max=item['release_year'].max(),
-                    value=[item['release_year'].min(), item['release_year'].max()],
-                    step=1,
-                    marks={
-                        item['release_year'].min():str(item['release_year'].min()),
-                        item['release_year'].max():str(item['release_year'].max()),
-                    },
-                    tooltip={"placement": "bottom", "always_visible": True},
-                    allowCross=False,
-                    id='selected_year'
-                ),
-                html.Br(),
-            ],
-            className='form-style'),
-        html.Div(
-            children=[
-                html.P('아니면 아이템 아이디(or 제목)를 입력해보라'),
-                html.P('특정 유저를 선택해서 그에 대한 추천리스트 자체를 고를 수도 있게 하자'),
-                html.Hr(),
-                html.Br(),
-            ],
-            className='form-style'),
-        dcc.Store(id='item_selected', storage_type='session') #데이터를 저장하는 부분
-    ],
-    className='sidebar'
-)
-
-emb = px.scatter(
-    item,
-    x = item['xs'],
-    y = item['ys'],
-    # color=item['selected'],
-    opacity=0.5
-    )
-
-emb.update_layout(clickmode='event+select')
-emb.update_traces()
-
-
-embedding = html.Div(
-    children=[
-        html.H3('아이템 2차원 임베딩'),
-        html.P('참고로 리랭킹 관련한 지원은 유저 페이지에서만 됩니다.'),
-        html.Br(),
-        dcc.Graph(
-            id='emb_graph',
-            figure=emb,
-            style={
-                'config.responsive': True
-            }
-            )
-    ]
-)
-
-side = html.Div(
-    children=[
-        html.H3('사이드인포'),
-        html.Br(),
-        html.Div(id='side_graph')
+        dbc.Row(
+            [dbc.Col(
+                html.Div(
+                    children=[
+                        # 년도, 장르, 제목?, 인기도
+                        html.P('장르'),
+                        dcc.Dropdown(
+                            options=[*uniq_genre],
+                            value=[],
+                            multi=True,
+                            id='selected_genre'
+                        ),
+                        html.P('년도'),
+                        dcc.RangeSlider(
+                            min=item['release_year'].min(),
+                            max=item['release_year'].max(),
+                            value=[item['release_year'].min(), item['release_year'].max()],
+                            step=1,
+                            marks={
+                                item['release_year'].min():str(item['release_year'].min()),
+                                item['release_year'].max():str(item['release_year'].max()),
+                            },
+                            tooltip={"placement": "bottom", "always_visible": True},
+                            allowCross=False,
+                            id='selected_year'
+                        ),
+                        html.Br(),
+                        dcc.Store(id='items_selected_by_option', storage_type='session'), #데이터를 저장하는 부분
+                        dcc.Store(id='items_selected_by_embed', storage_type='session') #데이터를 저장하는 부분
+                    ],
+                    className='form-style'),
+            ),
+            dbc.Col(
+                html.Div(
+                    children=[
+                        html.H3('아이템 2차원 임베딩'),
+                        html.P('참고로 리랭킹 관련한 지원은 유저 페이지에서만 됩니다.'),
+                        html.Br(),
+                        dcc.Graph(
+                            id='emb_graph',
+                            style={'config.responsive': True}
+                        )
+                    ]
+                )
+            ),
+            dbc.Col(
+                html.Div(
+                    children=[
+                        html.H3('사이드인포'),
+                        html.Br(),
+                        html.Div(id='side_graph')
+                    ]
+                )
+            ),]
+        )
     ]
 )
 
@@ -123,83 +96,105 @@ related_users = html.Div(
     ]
 )
 
-test = html.Div(
+
+layout = html.Div(
     children=[
-        html.H3(id='test', children='dd')
-    ]
+        gct.navbar,
+        selection,
+        related_users,
+    ],
+    # className='content'
 )
 
-layout = html.Div(children=[
-    gct.navbar,
-    sidebar,
-    embedding,
-    test,
-    side,
-    related_users,
-],className='content')
 
-# 옵션으로 선택한 것이 store에 저장되게 함
+# 옵션으로 선택한 아이템을 store1에 저장
 @callback(
-    Output('item_selected', 'data'),
+    Output('items_selected_by_option', 'data'),
     Input('selected_genre', 'value'), Input('selected_year', 'value'),
 )
-def store_selected_item(genre, year):
-    if genre is None:
-        raise PreventUpdate
-    if year is None:
-        raise PreventUpdate
-    # else:
-    #     item_lst = [i['pointIndex'] for i in emb['points']]
-    #     return item_lst
-    tmp = item.copy()
-    tmp = tmp[tmp['genre'].str.contains(
+def save_items_selected_by_option(genre, year):
+    print('storing by options')
+    item_lst = item.copy()
+    item_lst = item_lst[item_lst['genre'].str.contains(
         ''.join([*map(lambda x: f'(?=.*{x})', genre)]) + '.*', regex=True)]
-    tmp = tmp[(tmp['release_year'] >= year[0]) & (tmp['release_year'] <= year[1])]
-    return tmp.index.to_list()
+    item_lst = item_lst[(item_lst['release_year'] >= year[0]) & (item_lst['release_year'] <= year[1])]
+    return item_lst.index.to_list()
 
-# store에 저장된 것 or 임베딩에서 선택한 것이 사이드 그래프에 표시되게 함
+# embed graph에서 선택한 아이템을 store2에 저장
 @callback(
-    Output('side_graph', 'children'),
-    Input('item_selected', 'data'),
-    Input('emb_graph', 'selectedData'), # 각 인풋으로 이 콜백이 작동하는데 작동시킨 인풋을 가려내고 싶다
-)
-def change_side_graph(item_id, emb):
-    tmp = item.loc[item_id]
-    if emb is not None:
-        item_lst = [i['pointNumber'] for i in emb['points']]
-        tmp = item.iloc[item_lst]
-    year = px.histogram(tmp, x='release_year')
-    genre = px.histogram(tmp, x='genre')
-
-    return (dcc.Graph(figure=year), dcc.Graph(figure=genre))
-
-# store에 저장된 것으로 임베딩에 색깔 표시함
-@callback(
-    Output('emb_graph', 'figure'),
-    Input('item_selected', 'data'),
-)
-def change_emb_graph(item_id):
-    item['selected'] = 'Not Selected'
-    item.loc[item_id, 'selected'] = 'Selected'
-    emb = px.scatter(
-        item, x = 'xs', y = 'ys', color='selected',
-        opacity=0.9,
-        marginal_x="histogram",
-        marginal_y="histogram",
-        )
-    emb.update_layout(clickmode='event+select')
-    emb.update_traces()
-
-    return emb
-
-# # 그래프에서 선택한 것도 store에 저장하게 함
-@callback(
-    Output('test', 'children'),
+    Output('items_selected_by_embed', 'data'),
     Input('emb_graph', 'selectedData')
 )
-def choose_in_graph(data):
-    item_lst = None
-    if data is not None:
-        item_lst = [i['pointIndex'] for i in data['points']]
+def save_items_selected_by_embed(emb):
+    print('storing by embed')
+    if emb is None:
+        raise PreventUpdate
+    item_idx = [i['pointNumber'] for i in emb['points']]
+    item_lst = item.iloc[item_idx]
+    return item_lst.index.to_list()
 
-    return item_lst
+#어느 store든 최근에 저장된 store 기준으로 임베딩과 사이드 그래프를 그림
+@callback(
+    Output('side_graph', 'children'),
+    Output('emb_graph', 'figure'),
+    Input('items_selected_by_option', 'data'),
+    Input('items_selected_by_embed', 'data'),
+)
+def update_graph(store1, store2):
+    print(ctx.triggered_id)
+    if ctx.triggered_id == 'items_selected_by_option':
+
+        tmp = item.loc[store1]
+        year = px.histogram(tmp, x='release_year')
+        genre = px.histogram(tmp, x='genre')
+
+        item['selected'] = 'Not Selected'
+        item.loc[store1, 'selected'] = 'Selected'
+        emb = px.scatter(
+            item, x = 'xs', y = 'ys', color='selected', # 갯수에 따라 색깔이 유동적인 것 같다..
+            opacity=0.9,
+            marginal_x="histogram",
+            marginal_y="histogram",
+            )
+        emb.update_layout(
+            uirevision="emb_graph",
+            clickmode='event+select'
+            )
+        emb.update_traces()
+
+        return (dcc.Graph(figure=year), dcc.Graph(figure=genre)), emb
+    else:
+        item['selected'] = 'Not Selected'
+        if not store2:
+            raise PreventUpdate
+        emb = px.scatter(
+            item, x = 'xs', y = 'ys', color='selected', # 갯수에 따라 색깔이 유동적인 것 같다..
+            opacity=0.9,
+            marginal_x="histogram",
+            marginal_y="histogram",
+            )
+        emb.update_layout(
+            datarevision="emb_graph",
+            uirevision="emb_graph",
+            editrevision='emb_graph',
+            selectionrevision='emb_graph',
+            clickmode='event+select'
+            )
+        # emb.update_traces()
+        print(store2)
+
+        tmp = item.loc[store2]
+        year = px.histogram(tmp, x='release_year')
+        genre = px.histogram(tmp, x='genre')
+        return (dcc.Graph(figure=year), dcc.Graph(figure=genre)), emb
+
+
+# 임베딩에서 아이템 선택 시 옵션은 비활성화
+@callback(
+    Output('selected_genre', 'disabled'),
+    Output('selected_year', 'disabled'),
+    Input('items_selected_by_embed', 'data'),
+)
+def disable_options(value):
+    return False, False
+    return True, True
